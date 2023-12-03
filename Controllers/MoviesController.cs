c5076aae59a030536c20aba1a8779dd73bf06320
mohamedRebai <mohamedrebai748@gmail.com>
2023-12-03 14:46:10 +0100

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TP3.Models;
+
+namespace TP3.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MoviesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Movie.ToList());
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateMovie(Movie movie)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (movie.ImageFile != null && movie.ImageFile.Length > 0)
+                {
+                    var imagePath = Path.Combine("wwwroot/images", movie.ImageFile.FileName);
+                    using (var stream = new FileStream(imagePath, FileMode.Create))
+                    {
+                        movie.ImageFile.CopyTo(stream);
+                    }
+
+                    movie.Photo = $"/images/{movie.ImageFile.FileName}";
+                }
+
+                _db.Movie.Add(movie);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+            return View("Create", movie);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var movie = _db.Movie.Find(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+
+
+            return View(movie);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                if (movie.ImageFile != null && movie.ImageFile.Length > 0)
+                {
+                    if (!string.IsNullOrEmpty(movie.Photo))
+                    {
+                        var filename = Path.Combine("wwwroot", movie.Photo.TrimStart('/'));
+
+                        if (System.IO.File.Exists(filename))
+                        {
+                            System.IO.File.Delete(filename);
+                        }
+                    }
+
+
+                    var imagePath = Path.Combine("wwwroot/images", movie.ImageFile.FileName);
+                    using (var stream = new FileStream(imagePath, FileMode.Create))
+                    {
+                        movie.ImageFile.CopyTo(stream);
+                    }
+
+                    movie.Photo = $"/images/{movie.ImageFile.FileName}";
+                }
+                _db.Entry(movie).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movie);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var movie = _db.Movie.Find(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var movie = _db.Movie.Find(id);
+
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            if (!string.IsNullOrEmpty(movie.Photo))
+            {
+                var imagePath = Path.Combine("wwwroot", movie.Photo.TrimStart('/'));
+
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+            }
+
+
+            _db.Movie   .Remove(movie);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null) return Content("unable to find Id");
+            var c = _db.Movie.SingleOrDefault(c => c.Id == id);
+            return View(c);
+        }
+    }
+}
